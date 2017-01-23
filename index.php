06de526729a9485112d395d4d3f5ec2f59800589
fvargasestrada <paco@sauz.es>
2017-01-23 12:55:30 +0100

    base preparada y portada hecha

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Elena ETC</title>
+
+    <?php include('includes/head.html'); ?>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="row flex-parent alto-total">
+			<div class="col-xs-12 text-center flex-child">
+				<a href="#">
+					<img src="img/logo-portada.png" class="img-responsive center-block" />
+				</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
