bb7304ff6d10e79a9fe32fcde166c1ad49d37837
fvargasestrada <paco@sauz.es>
2017-01-24 17:38:57 +0100

    header y footer en includes

--- elena-franco-alvarez.php
+++ elena-franco-alvarez.php
@@ -8,46 +8,7 @@
 </head>
 
 <body>
-	<div class="container" id="menu-principal">
-		<nav class="navbar navbar-default" role="navigation">
-		  <!-- El logotipo y el icono que despliega el menú se agrupan
-		       para mostrarlos mejor en los dispositivos móviles -->
-		  <div class="navbar-header">
-		    <button type="button" class="navbar-toggle" data-toggle="collapse"
-		            data-target=".navbar-ex1-collapse">
-		      <span class="sr-only">Desplegar navegación</span>
-		      <span class="icon-bar"></span>
-		      <span class="icon-bar"></span>
-		      <span class="icon-bar"></span>
-		    </button>
-		    <a class="navbar-brand" href="#"><img src="img/logo-menu.jpg" alt="ETC" /></a>
-		  </div>
-		 
-		  <!-- Agrupar los enlaces de navegación, los formularios y cualquier
-		       otro elemento que se pueda ocultar al minimizar la barra -->
-		  <div class="collapse navbar-collapse navbar-ex1-collapse">
-		    <ul class="nav navbar-nav">
-		      <li class="active"><a href="elena-franco-alvarez.php">Elena Franco Álvarez</a></li>
-		      <li><a href="#">que hacemos</a></li>
-		      <li><a href="#">como lo hacemos</a></li>
-		      <li><a href="#">para quien lo hacemos</a></li>
-		      <li><a href="#">contacto</a></li>
-		    </ul>
-		  </div>
-
-			<div class="visible-lg c2a-encargo-lg">
-				<a href="#"><img src="img/c2a-encargo.png" alt="Rellene aquí la hoja de encargo" /></a>
-			</div>
-
-			<div class="visible-sm visible-md c2a-encargo-md">
-				<a href="#"><img src="img/c2a-encargo-horizontal.png" alt="Rellene aquí la hoja de encargo" class="img-responsive" /></a>
-			</div>
-
-			<div class="visible-xs c2a-encargo-md">
-				<a href="#"><img src="img/c2a-encargo-movil.png" alt="Rellene aquí la hoja de encargo" class="img-responsive" /></a>
-			</div>
-		</nav>
-	</div>
+	<?php include('includes/header.html'); ?>
 
 	<div class="container cuerpo">
 		<div class="row">
@@ -57,31 +18,6 @@
 		</div>
 	</div>
 
-	<div class="container text-center">
-		<footer>
-			<div class="fila-footer hidden-xs">
-				Elena Franco Álvarez<span class="separador"></span>Consultoría Administrativa Especializada<span class="separador"></span>+34 666 240 934<span class="separador"></span>www.elenaetc.es<span class="separador"></span>elena@elenaetc.es
-			</div>
-
-			<div class="fila-footer visible-xs textos-footer-movil text-left">
-				<p>Elena Franco Álvarez</p>
-				<p>Consultoría Administrativa Especializada</p>
-				<p>+34 666 240 934</p>
-				<p>www.elenaetc.es<span class="separador"></span>elena@elenaetc.es</p>
-			</div>
-
-			<div class="fila-footer">
-				<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-				<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-				<a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-				<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-			</div>
-
-			<div class="fila-footer enlaces-footer">
-				<a href="#">aviso legal</a>
-				<a href="#">política de privacidad</a>
-			</div>
-		</footer>
-	</div>
+	<?php include('includes/footer.html'); ?>
 </body>
 </html>
