85b1c23acc9b50bb37aaf7a992bd8e69d56607a4
fvargasestrada <paco@sauz.es>
2017-01-24 12:20:27 +0100

    menú terminado

--- elena-franco-alvarez.php
+++ elena-franco-alvarez.php
@@ -8,28 +8,41 @@
 </head>
 
 <body>
-	<nav class="navbar navbar-default" role="navigation">
-	  <!-- El logotipo y el icono que despliega el menú se agrupan
-	       para mostrarlos mejor en los dispositivos móviles -->
-	  <div class="navbar-header">
-	    <button type="button" class="navbar-toggle" data-toggle="collapse"
-	            data-target=".navbar-ex1-collapse">
-	      <span class="sr-only">Desplegar navegación</span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	    </button>
-	    <a class="navbar-brand" href="#"><img src="img/logo-menu.jpg" alt="ETC" /></a>
-	  </div>
-	 
-	  <!-- Agrupar los enlaces de navegación, los formularios y cualquier
-	       otro elemento que se pueda ocultar al minimizar la barra -->
-	  <div class="collapse navbar-collapse navbar-ex1-collapse">
-	    <ul class="nav navbar-nav">
-	      <li class="active"><a href="#">Enlace #1</a></li>
-	      <li><a href="#">Enlace #2</a></li>
-	    </ul>
-	  </div>
-	</nav>
+	<div class="container" id="menu-principal">
+		<nav class="navbar navbar-default" role="navigation">
+		  <!-- El logotipo y el icono que despliega el menú se agrupan
+		       para mostrarlos mejor en los dispositivos móviles -->
+		  <div class="navbar-header">
+		    <button type="button" class="navbar-toggle" data-toggle="collapse"
+		            data-target=".navbar-ex1-collapse">
+		      <span class="sr-only">Desplegar navegación</span>
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		      <span class="icon-bar"></span>
+		    </button>
+		    <a class="navbar-brand" href="#"><img src="img/logo-menu.jpg" alt="ETC" /></a>
+		  </div>
+		 
+		  <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+		       otro elemento que se pueda ocultar al minimizar la barra -->
+		  <div class="collapse navbar-collapse navbar-ex1-collapse">
+		    <ul class="nav navbar-nav">
+		      <li class="active"><a href="elena-franco-alvarez.php">Elena Franco Álvarez</a></li>
+		      <li><a href="#">que hacemos</a></li>
+		      <li><a href="#">como lo hacemos</a></li>
+		      <li><a href="#">para quien lo hacemos</a></li>
+		      <li><a href="#">contacto</a></li>
+		    </ul>
+		  </div>
+		</nav>
+	</div>
+
+	<div class="container cuerpo">
+		<div class="row">
+			<div class="col-xs-12">
+				Aquí el cuerpo de la página
+			</div>
+		</div>
+	</div>
 </body>
 </html>
