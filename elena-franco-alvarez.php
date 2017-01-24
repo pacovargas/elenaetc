45473f928e799be1719804fa0e2ded66e00c96d2
fvargasestrada <paco@sauz.es>
2017-01-24 18:07:58 +0100

    principal terminada

--- elena-franco-alvarez.php
+++ elena-franco-alvarez.php
@@ -11,9 +11,29 @@
 	<?php include('includes/header.html'); ?>
 
 	<div class="container cuerpo">
-		<div class="row">
+		<div class="row text-center titulo">
 			<div class="col-xs-12">
-				Aquí el cuerpo de la página
+				<img src="img/titulo-principal.png" alt="ELENA FRANCO ÁLVAREZ" />
+			</div>
+		</div>
+
+		<div class="row texto-principal">
+			<div class="col-sm-7">
+				<p>Después de 15 años trabajando en el sector de la construcción y la promoción inmobiliaria, por cuenta ajena, la experiencia adquirida en el campo específico de la gestión administrativa y de gestión en el ámbito del sector de la construcción me ha permitido perfeccionar un método de trabajo efectivo, basado en la profundidad de conocimientos acumulados tras afrontar retor de un perfil muy diverso en el ámbito de un sector tan heterogéneo como el de la promoción inmobiliaria.</p>
+
+				<p>ETC ha nacido para poner todo este Know How al servicio de las empresas del sector.</p>
+
+				<p>Soy una persona ordenada, concienzuda, eficaz y metódica, con gran flexibilidad y adaptabilidad a los distintos tipos y tiempos de trabajo, considerándome capaz de hacer ahorrar a tu empresa en tiempo y costes. Será para mí una satisfacción contribuir a que puedas concentrarte en tu trabajo, dejándome a mí, a ETC, todo lo demás.</p>
+
+				<p>El sector de la construcción en general ya no es el mismo. La especialización y la profesionalidad de las empresas y técnicos que intervienen en el mismo ya no es un factor de diferenciación, de competitividad, sino de supervivencia.</p>
+
+				<p class="naranja">¿Está tu empresa? ¿Tienes recursos y medios suficientes? ¿Necesitas ayuda?</p>
+
+				<p class="text-right"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></p>
+			</div>
+
+			<div class="col-sm-5">
+				<img src="img/retrato-elena.jpg" class="img-responsive" />
 			</div>
 		</div>
 	</div>
