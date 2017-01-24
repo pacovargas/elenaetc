29775b25df7648438a3bff3c64b9e11998a0aba6
fvargasestrada <paco@sauz.es>
2017-01-24 19:20:04 +0100

    cabecera que hacemos

--- que-hacemos.php
+++ que-hacemos.php
@@ -13,7 +13,18 @@
 	<?php include('includes/header.html'); ?>
 
 	<div class="container cuerpo">
-		qué hacemos
+		<div class="row">
+			<div class="col-sm-3">
+				<img src="img/logo-muneco.png" class="img-responsive center-block" />
+			</div>
+			<div class="col-sm-3" id="titulo-que-hacemos">
+				<img src="img/titulo-que-hacemos.png" class="img-responsive" alt="QUE HACEMOS" />
+			</div>
+			<div class="col-sm-6 champan-bold" id="etcetera">
+				<p class="subtitulo"><span class="naranja mays">ETC</span> Del lat. et ceterea &#8220;y todo lo demás&#8221;.</p>
+				<p>1.expr. U. para sustituir el resto de una exposición o enumeración que se sobreentiende. Se emplea generalmente en la abreviatura etc.</p>
+			</div>
+		</div>
 	</div>
 
 	<?php include('includes/footer.html'); ?>
