48798dafc6191227879854b14bc2658d73fec7c4
fvargasestrada <paco@sauz.es>
2017-01-25 10:27:57 +0100

    responsive de como lo hacemos terminado

--- como-lo-hacemos.php
+++ como-lo-hacemos.php
@@ -9,10 +9,10 @@
     <?php include('includes/head.html'); ?>
 </head>
 
-<body class="que-hacemos">
+<body class="como-lo-hacemos">
     <?php include('includes/header.html'); ?>
 
-    <div class="container cuerpo carrusel-individual">
+    <div class="container cuerpo carrusel-individual hidden-xs">
         <div class="row como-carr1">
             <div class="col-sm-3 como-izq hidden-xs">
                 <div class="muneco-como">
@@ -82,6 +82,91 @@
                 <div class="titulo champan-bold mays">
                     Como <span class="naranja">lo</span> <span class="gris">hacemos</span>
                 </div>
+                <div class="text-right helvetica-bold-italic">
+                    &#8220;La externalización de trámites como la gestión de una Licencia, un permiso, Alta de Bienes Inmuebles o la gestión de un activo en su conjunto, conlleva un ahorro de tiempo, coste y estructura para su empresa.&#8221;
+                </div>
+                <div class="text-right helvetica-light firma">
+                    Elena Franco Álvarez CEO de ETC.
+                </div>
+            </div>
+            <div class="col-sm-12 como-carr2-textos">
+                <p class="helvetica">En ETC nuestro principal objetivo es que el cliente continue con su trabajo y nosotros nos encargamos de todo los demás, así ofrecemos el mejor servicio con la mejor especialización.</p>
+                <p class="helvetica">Externalizar significa para nosotros la Optimización de Tiempo, Recursos y Costes de cara a nuestros clientes, además <span class="naranja helvetica-bold">somos flexibles y nos adaptamos a tu método de trabajo</span>.</p>
+            </div>
+            <div class="col-sm-12 como-carr2-imagenes">
+                <img src="img/relojes.jpg" class="img-responsive center-block" />
+            </div>
+        </div>
+    </div>
+
+    <div class="container cuerpo visible-xs">
+        <div class="row como-carr1">
+            <div class="col-sm-3 como-izq hidden-xs">
+                <div class="muneco-como">
+                    <img src="img/logo-muneco-bombilla.png" class="img-responsive" />
+                </div>
+                <div class="imagenes-como">
+                    <img src="img/como1.jpg" class="img-responsive" />
+                </div>
+                <div class="imagenes-como">
+                    <img src="img/como2.jpg" class="img-responsive" />
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-9 como-der">
+                <div class="titulo champan-bold mays">
+                    Como <span class="naranja">lo</span> <span class="gris">hacemos</span>
+                </div>
+                <hr />
+
+                <div class="punto-como helvetica-light mays">
+                    <span class="naranja">01</span>.¿Qué podemos hacer por ti?
+                </div>
+                <div class="explicacion-como">
+                    Detección/Diagnóstico de necesidades del cliente. Hoja de encargo a rellenar por el cliente a través de un formulario.
+                </div>
+                <hr />
+
+                <div class="punto-como helvetica-light mays">
+                    <span class="naranja">02</span>.Contacto personal
+                </div>
+                <div class="explicacion-como">
+                    Primera toma de contacto.
+                </div>
+                <hr />
+
+                <div class="punto-como helvetica-light mays">
+                    <span class="naranja">03</span>.En marcha
+                </div>
+                <div class="explicacion-como">
+                    Formalización de la contratación de servicios entre el cliente y ETC.
+                </div>
+                <hr />
+
+                <div class="punto-como helvetica-light mays">
+                    <span class="naranja">04</span>.Seguimiento
+                </div>
+                <div class="explicacion-como">
+                    ETC. se encarga del seguimiento total de todos los trámites e informa al cliente del proceso y tiempos.
+                </div>
+                <hr />
+
+                <div class="punto-como helvetica-light mays">
+                    <span class="naranja">05</span>.Resultados
+                </div>
+                <div class="explicacion-como">
+                    Ya es realidad su ahorro en tiempo y costes, ETC. le hace entrega de toda la documentación relativa a su expediente con la tranquilidad de tenerlos en tiempo y forma según lo acordado en la hoja de encargo.
+                </div>
+                <hr />
+            </div>
+        </div>
+
+        <div class="row como-carr2">
+            <div class="col-sm-3 como-izq hidden-xs">
+                <div class="muneco-como">
+                    <img src="img/logo-muneco-bombilla.png" class="img-responsive" />
+                </div>
+            </div>
+            <div class="col-sm-9 como-der">
                 <div class="text-right helvetica-bold-italic">
                     &#8220;La externalización de trámites como la gestión de una Licencia, un permiso, Alta de Bienes Inmuebles o la gestión de un activo en su conjunto, conlleva un ahorro de tiempo, coste y estructura para su empresa.&#8221;
                 </div>
