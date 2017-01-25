2bd08b8013bc1a1d7e4c384bca4dae9b9bbe1c70
fvargasestrada <paco@sauz.es>
2017-01-25 12:25:59 +0100

    para quien terminado con responsive incluído

---
+++ para-quien-lo-hacemos.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>Elena ETC - Consultoría Administrativa Especializada</title>
+
+    <script type="text/javascript">var menu_nbr = 4;</script>
+
+    <?php include('includes/head.html'); ?>
+</head>
+
+<body class="para-quien-lo-hacemos">
+    <?php include('includes/header.html'); ?>
+
+    <div class="container cuerpo">
+        <div class="row">
+            <div class="col-sm-3 para-izq hidden-xs">
+                <div class="muneco-para">
+                    <img src="img/logo-dos-munecos.png" class="img-responsive" />
+                </div>
+            </div>
+            <div class="col-sm-9 para-der">
+                <div class="titulo champan-bold mays">
+                    Para <span class="naranja">quien</span> lo <span class="gris">hacemos,</span> por qué
+                </div>
+                <div class="text-right helvetica-italic cita">
+                    &#8220;ETC. le ofrece un beneficio emocional al despreocuparse y liberarse de trámites a través de nuestra asesoría administrativa especializada.&#8221;
+                </div>
+                <div class="text-right helvetica-bold firma">
+                    Elena Franco Álvarez CEO de ETC.
+                </div>
+            </div>
+        </div>
+        <hr />
+    </div>
+
+    <div class="container cuerpo carrusel-individual hidden-xs">
+        <div class="row para-carr-1">
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Profesionales Técnicos</h2>
+                <p>Porque con ETC. los Estudios de Arquitectura, Ingeniería y Aparejadores estarán volcados en su trabajo y no en las gestiones.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/profesionales-tecnicos.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Promotoras Inmobiliarias</h2>
+                <p>Porque concentrar trámites especializados a través de ETC. mientras vuelca sus esfuerzos en la promoción de sus inmuebles.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/promotoras-inmobiliarias.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Constructoras</h2>
+                <p>Porque grandes proyectos requieren soluciones que ahorren costes y tiempo.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/constructuras.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Entidades Financieras</h2>
+                <p>Porque minimiza los riesgos derivados en coste y tiempo de la falta de especialización del ámbito inmobiliario.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/entidades-financieras.png" class="img-responsive center-block" />
+            </div>
+        </div>
+
+        <div class="row para-carr-2">
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Comunidades de Propietarios <span class="negro">|</span> Administradores de fincas</h2>
+                <p>Porque delegas las gestiones más importantes en un profesional del sector.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/comunidades.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Establecimientos Comerciales</h2>
+                <p>Porque seas un pequeño comercio, una oficina mediana o una gran empresa, encontramos soluciones a tu medida.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/establecimientos.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Particulares</h2>
+                <p>Porque su proyecto particular, sus ilusiones o sueños... en ETC. los analizamos y convertimos en realidad.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/particulares.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Etcétera...</h2>
+                <p>Si su perfil no está aquí, no dude en contactar con nosotros para conocer sus necesidades específicas.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/etcetera.png" class="img-responsive center-block" />
+            </div>
+        </div>
+    </div>
+
+    <div class="container cuerpo visible-xs">
+        <div class="row para-carr-1">
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Profesionales Técnicos</h2>
+                <p>Porque con ETC. los Estudios de Arquitectura, Ingeniería y Aparejadores estarán volcados en su trabajo y no en las gestiones.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/profesionales-tecnicos.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Promotoras Inmobiliarias</h2>
+                <p>Porque concentrar trámites especializados a través de ETC. mientras vuelca sus esfuerzos en la promoción de sus inmuebles.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/promotoras-inmobiliarias.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Constructoras</h2>
+                <p>Porque grandes proyectos requieren soluciones que ahorren costes y tiempo.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/constructuras.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Entidades Financieras</h2>
+                <p>Porque minimiza los riesgos derivados en coste y tiempo de la falta de especialización del ámbito inmobiliario.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/entidades-financieras.png" class="img-responsive center-block" />
+            </div>
+        </div>
+
+        <div class="row para-carr-2">
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Comunidades de Propietarios <span class="negro">|</span> Administradores de fincas</h2>
+                <p>Porque delegas las gestiones más importantes en un profesional del sector.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/comunidades.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Establecimientos Comerciales</h2>
+                <p>Porque seas un pequeño comercio, una oficina mediana o una gran empresa, encontramos soluciones a tu medida.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/establecimientos.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Particulares</h2>
+                <p>Porque su proyecto particular, sus ilusiones o sueños... en ETC. los analizamos y convertimos en realidad.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/particulares.png" class="img-responsive center-block" />
+            </div>
+            <div class="col-xs-12 para-listado para-listado-separador"></div>
+            <div class="col-xs-12 col-sm-12 col-md-10 para-listado para-listado-texto">
+                <h2 class="naranja helvetica-light">Etcétera...</h2>
+                <p>Si su perfil no está aquí, no dude en contactar con nosotros para conocer sus necesidades específicas.</p>
+            </div>
+            <div class="col-md-2 para-listado-icono para-listado hidden-xs hidden-sm">
+                <img src="img/etcetera.png" class="img-responsive center-block" />
+            </div>
+        </div>
+    </div>
+
+    <?php include('includes/footer.html'); ?>
+</body>
+</html>
