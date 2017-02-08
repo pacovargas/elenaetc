2ee537e29c403faddaa8a4673d3d1ac0af408acf
fvargasestrada <paco@sauz.es>
2017-02-08 09:51:08 +0100

    corrección textos portada

--- elena-franco-alvarez.php
+++ elena-franco-alvarez.php
@@ -21,7 +21,7 @@
 
 		<div class="row texto-principal">
 			<div class="col-sm-7">
-				<p>Después de 15 años trabajando en el sector de la construcción y la promoción inmobiliaria, por cuenta ajena, la experiencia adquirida en el campo específico de la gestión administrativa y de gestión en el ámbito del sector de la construcción me ha permitido perfeccionar un método de trabajo efectivo, basado en la profundidad de conocimientos acumulados tras afrontar retor de un perfil muy diverso en el ámbito de un sector tan heterogéneo como el de la promoción inmobiliaria.</p>
+				<p>Después de más 15 años trabajando en el sector de la construcción y la promoción inmobiliaria, por cuenta ajena, la experiencia adquirida en el específico campo de la tramitación administrativa y de gestión me ha permitido perfeccionar un método de trabajo efectivo, basado en la profundidad de conocimientos acumulados tras afrontar retos de un perfil muy diverso en el ámbito de un sector tan heterogéneo como el de la promoción inmobiliaria.</p>
 
 				<p>ETC ha nacido para poner todo este Know How al servicio de las empresas del sector.</p>
 
@@ -29,7 +29,7 @@
 
 				<p>El sector de la construcción en general ya no es el mismo. La especialización y la profesionalidad de las empresas y técnicos que intervienen en el mismo ya no es un factor de diferenciación, de competitividad, sino de supervivencia.</p>
 
-				<p class="naranja">¿Está tu empresa? ¿Tienes recursos y medios suficientes? ¿Necesitas ayuda?</p>
+				<p class="naranja">¿Está tu empresa preparada? ¿Tienes recursos y medios suficientes? ¿Necesitas ayuda?</p>
 
 				<p class="text-right centrado-movil"><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></p>
 			</div>
