c9d58e622c6280b7acfa0a4e0fff25a7a16a94ee
fvargasestrada <paco@sauz.es>
2017-03-02 17:52:01 +0100

    estilos a la tabla de admin terminado

--- property-management/templates/admin.tpl
+++ property-management/templates/admin.tpl
@@ -45,17 +45,17 @@
 						{foreach from=$propiedades item=prop}
 							{if $prop->activa == $mostrar_activas || $mostrar_activas > 1}
 								<tr>
-									<td>{$prop->id}</td>
+									<td class="text-center">{$prop->id}</td>
 									<td>{$prop->nombre}</td>
 									<td>{$prop->referencia}</td>
 									<td>{$prop->nombre_provincia}</td>
 									<td>{$prop->nombre_municipio}</td>
 									<td>{$prop->nombre_regimen}</td>
-									<td>{$prop->precio}€</td>
-									<td>{$prop->fecha}</td>
-									<td>{if $prop->activa == 1}Sí{else}No{/if}</td>
-									<td><a href="{$current_url}borrar={$prop->id}"><i class="fa fa-times" aria-hidden="true"></i></a></td>
-									<td><a href="{$current_url}accion=actualizar&propiedad={$prop->id}"><i class="fa fa-pencil-square" aria-hidden="true"></i></a></td>
+									<td class="text-right">{$prop->precio}€</td>
+									<td class="text-right">{$prop->fecha}</td>
+									<td class="text-center">{if $prop->activa == 1}Sí{else}No{/if}</td>
+									<td class="text-center"><a href="{$current_url}borrar={$prop->id}"><i class="fa fa-times" aria-hidden="true"></i></a></td>
+									<td class="text-center"><a href="{$current_url}accion=actualizar&propiedad={$prop->id}"><i class="fa fa-pencil-square" aria-hidden="true"></i></a></td>
 								</tr>
 							{/if}
 						{/foreach}
--- property-management/templates_c/61210d1a49150fc6cba396a78b7c47728c2dfda6_0.file.admin.tpl.php
+++ property-management/templates_c/61210d1a49150fc6cba396a78b7c47728c2dfda6_0.file.admin.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-03-02 17:25:18
+/* Smarty version 3.1.30, created on 2017-03-02 17:49:35
   from "/home/paco/repos/elenaetc/property-management/templates/admin.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58b8476e2d6dd1_62117735',
+  'unifunc' => 'content_58b84d1f652263_66527575',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '61210d1a49150fc6cba396a78b7c47728c2dfda6' => 
     array (
       0 => '/home/paco/repos/elenaetc/property-management/templates/admin.tpl',
-      1 => 1488471907,
+      1 => 1488473368,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58b8476e2d6dd1_62117735 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58b84d1f652263_66527575 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 <div class="container-fluid cuerpo-admin">
 	<div class="row">
@@ -74,7 +74,7 @@ foreach ($_from as $_smarty_tpl->tpl_vars['prop']->value) {
 ?>
 							<?php if ($_smarty_tpl->tpl_vars['prop']->value->activa == $_smarty_tpl->tpl_vars['mostrar_activas']->value || $_smarty_tpl->tpl_vars['mostrar_activas']->value > 1) {?>
 								<tr>
-									<td><?php echo $_smarty_tpl->tpl_vars['prop']->value->id;?>
+									<td class="text-center"><?php echo $_smarty_tpl->tpl_vars['prop']->value->id;?>
 </td>
 									<td><?php echo $_smarty_tpl->tpl_vars['prop']->value->nombre;?>
 </td>
@@ -86,15 +86,15 @@ foreach ($_from as $_smarty_tpl->tpl_vars['prop']->value) {
 </td>
 									<td><?php echo $_smarty_tpl->tpl_vars['prop']->value->nombre_regimen;?>
 </td>
-									<td><?php echo $_smarty_tpl->tpl_vars['prop']->value->precio;?>
+									<td class="text-right"><?php echo $_smarty_tpl->tpl_vars['prop']->value->precio;?>
 €</td>
-									<td><?php echo $_smarty_tpl->tpl_vars['prop']->value->fecha;?>
+									<td class="text-right"><?php echo $_smarty_tpl->tpl_vars['prop']->value->fecha;?>
 </td>
-									<td><?php if ($_smarty_tpl->tpl_vars['prop']->value->activa == 1) {?>Sí<?php } else { ?>No<?php }?></td>
-									<td><a href="<?php echo $_smarty_tpl->tpl_vars['current_url']->value;?>
+									<td class="text-center"><?php if ($_smarty_tpl->tpl_vars['prop']->value->activa == 1) {?>Sí<?php } else { ?>No<?php }?></td>
+									<td class="text-center"><a href="<?php echo $_smarty_tpl->tpl_vars['current_url']->value;?>
 borrar=<?php echo $_smarty_tpl->tpl_vars['prop']->value->id;?>
 "><i class="fa fa-times" aria-hidden="true"></i></a></td>
-									<td><a href="<?php echo $_smarty_tpl->tpl_vars['current_url']->value;?>
+									<td class="text-center"><a href="<?php echo $_smarty_tpl->tpl_vars['current_url']->value;?>
 accion=actualizar&propiedad=<?php echo $_smarty_tpl->tpl_vars['prop']->value->id;?>
 "><i class="fa fa-pencil-square" aria-hidden="true"></i></a></td>
 								</tr>
